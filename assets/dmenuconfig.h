5c8a59cab3c21c76e7836c7f7efcf73adcd64a54
Maurycy Z <10maurycy10@gmail.com>
2021-12-28 14:50:40 -0800

    fixes

--- assets/dmenuconfig.h
+++ assets/dmenuconfig.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=10",
 };
 
 #define BLUE "#2266FF"
