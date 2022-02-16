be321469db47c62e31f447ef9ec22371046b6f0c
Maurycy Z <10maurycy10@gmail.com>
2022-02-16 12:26:41 -0800

    allow ofline running of clipman buildscript

--- install.d/clipman.sh
+++ install.d/clipman.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-
 export GOPATH="$HOME/.local/share/go"
 export GOROOT="/usr/lib/go"
 export GOBIN="$GOPATH/bin"
 
-go get github.com/yory8/clipman
+mkdir buildtmp
+pushd buildtmp
+tar -xzvf ../arc/clipman.tar.gz
+cd clipman
+go install
+popd
+rm -rf buildtmp
 
